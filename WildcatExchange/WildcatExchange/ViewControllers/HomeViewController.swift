cc596df27b730c8891422c507ce8fd3c689ca0fa
Kvyas2024 <78030160+Kvyas2024@users.noreply.github.com>
2023-11-16 23:31:37 -0500

    Update HomeViewController.swift
    reads multiple entries from database in json format and scroll feature

--- WildcatExchange/WildcatExchange/ViewControllers/HomeViewController.swift
+++ WildcatExchange/WildcatExchange/ViewControllers/HomeViewController.swift
@@ -1,27 +1,9 @@
-
-
 //
 //  HomeViewController.swift
 //  WildcatExchange
 //
 //  Created by Kishan Vyas on 11/7/23.
 //
-/*
-
-
-import Foundation
-import UIKit
-
-class HomeViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .white
-        self.title = "Marketplace"
-    }
-    
-}
-*/
 
 
 
